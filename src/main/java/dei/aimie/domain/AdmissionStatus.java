72ff5231ae2c035d0e5d022342268b40baeafe53
D <deilee0913@gmail.com>
2025-07-17 14:32:02 +0900

    update: AdmissionStatus 클래스 displayName 필드 및 getter 추가

--- src/main/java/dei/aimie/domain/AdmissionStatus.java
+++ src/main/java/dei/aimie/domain/AdmissionStatus.java
@@ -1,7 +1,17 @@
 package dei.aimie.domain;
 
 public enum AdmissionStatus {
-    ENROLLED,     // 재학 중
-    ON_LEAVE,     // 휴학 중
-    GRADUATED     // 졸업
+    ENROLLED("재학 중"),
+    ON_LEAVE("휴학 중"),
+    GRADUATED("졸업");
+
+    private final String displayName;
+
+    AdmissionStatus(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
 }
